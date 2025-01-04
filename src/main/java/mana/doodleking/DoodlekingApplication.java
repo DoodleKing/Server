8c2b07e683bbd57a53847c9395ca4842f81dd1bb
성유진 <syj010820@naver.com>
2025-01-04 19:53:06 +0900

    chore: init project

---
+++ src/main/java/mana/doodleking/DoodlekingApplication.java
@@ -0,0 +1,13 @@
+package mana.doodleking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodlekingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodlekingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mana/doodleking/DoodlekingApplicationTests.java
@@ -0,0 +1,13 @@
+package mana.doodleking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodlekingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
