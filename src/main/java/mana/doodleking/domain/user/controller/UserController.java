f313235264c6dd80201310caeac033e51bd03842
KSJ0128 <ksj01128@naver.com>
2025-01-10 02:07:00 +0900

    refactor: 유저 생성 API 응답 값 수정
    유저 생성 API 응답 값 APIResponse를 반영해 수정

--- src/main/java/mana/doodleking/domain/user/controller/UserController.java
+++ src/main/java/mana/doodleking/domain/user/controller/UserController.java
@@ -1,14 +1,16 @@
 package mana.doodleking.domain.user.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mana.doodleking.domain.user.dto.CreateUserReq;
 import mana.doodleking.domain.user.dto.CreateUserRes;
 import mana.doodleking.domain.user.service.UserService;
+import mana.doodleking.global.response.APIResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -17,12 +19,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("")
-    private CreateUserRes userCreate(@RequestBody CreateUserReq createUserReq) throws Exception {
+    private ResponseEntity<APIResponse<CreateUserRes>> userCreate(@RequestBody @Valid CreateUserReq createUserReq) throws Exception {
         // 유저명 중복 검증
         userService.validUserName(createUserReq.getUserName());
         // 캐릭터 ID 존재 검증
         userService.validCharacter(createUserReq.getCharacterId());
 
-        return CreateUserRes.from(userService.createUser(createUserReq));
+        return ResponseEntity.ok(APIResponse.success(CreateUserRes.from(userService.createUser(createUserReq))));
     }
 }
