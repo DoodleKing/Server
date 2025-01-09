4a68fb1492d31ea120aa0bb787f923f83f5dd05b
KSJ0128 <ksj01128@naver.com>
2025-01-09 22:13:41 +0900

    feat: UserRepository 생성
    유저명 존재 여부 확인 메서드 구현

---
+++ src/main/java/mana/doodleking/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package mana.doodleking.domain.user.repository;
+
+import mana.doodleking.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByName(String name);
+}
