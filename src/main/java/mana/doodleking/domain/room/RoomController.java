be00b35dd744d081a50f3b28675b79e2483ec60a
syj <syj010820@naver.com>
2025-01-12 00:55:29 +0900

    feat: 구독목록 분리하여 웹소켓 응답메시지 전달

--- src/main/java/mana/doodleking/domain/room/RoomController.java
+++ src/main/java/mana/doodleking/domain/room/RoomController.java
@@ -1,9 +1,15 @@
 package mana.doodleking.domain.room;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import mana.doodleking.domain.room.dto.RoomDetail;
 import mana.doodleking.domain.room.dto.PostRoomReq;
 import mana.doodleking.domain.room.dto.RoomSimple;
+import mana.doodleking.websocket.RequestChatContentsDto;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +19,10 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping("/api/room")
 @RestController
+@Slf4j
 public class RoomController {
     private final RoomService roomService;
-
+    private final SimpMessagingTemplate messagingTemplate;
     @PostMapping
     public RoomDetail createRoom(@RequestBody PostRoomReq postRoomReq) {
         Room createdRoom = roomService.createRoom(postRoomReq);
@@ -28,5 +35,22 @@ public class RoomController {
         return roomList.stream()
                 .map(RoomSimple::from)
                 .toList();
+    }
+    @MessageMapping("/init")
+    @SendTo("/topic/init")
+    public RequestChatContentsDto init(@Header("simpSessionId") String sessionId, RequestChatContentsDto message) {
+        message.setContents(sessionId);
+        return message;
+    }
+
+    @MessageMapping("/test")
+    @SendTo("/topic/lobby")
+    public RequestChatContentsDto sendMessage(@Header("simpSessionId") String userId, RequestChatContentsDto message) {
+        List<Room> roomList = roomService.getRoomList();
+        String dest = "/topic/my/" + userId;
+        log.info(dest);
+        messagingTemplate.convertAndSend(dest, message);
+        message.setContents(userId + " " + roomList.toString());
+        return message;
     }
 }
