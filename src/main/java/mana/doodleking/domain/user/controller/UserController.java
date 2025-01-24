7a0dfa2e74624164df91e62fa599e3f783c4b8a3
syj <syj010820@naver.com>
2025-01-24 15:53:14 +0900

    docs: UserController 문서 작성

--- src/main/java/mana/doodleking/domain/user/controller/UserController.java
+++ src/main/java/mana/doodleking/domain/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package mana.doodleking.domain.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mana.doodleking.domain.user.dto.CreateUserReq;
 import mana.doodleking.domain.user.dto.CreateUserRes;
 import mana.doodleking.domain.user.service.UserService;
 import mana.doodleking.global.response.APIResponse;
+import mana.doodleking.global.swagger.UserControllerDocs;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,11 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements UserControllerDocs {
     private final UserService userService;
 
-    @PostMapping("")
-    private ResponseEntity<APIResponse<CreateUserRes>> userCreate(@RequestBody @Valid CreateUserReq createUserReq) throws Exception {
+    @Operation(summary = "사용자 생성")
+    @PostMapping
+    public ResponseEntity<APIResponse<CreateUserRes>> userCreate(@RequestBody @Valid CreateUserReq createUserReq) throws Exception {
         // 유저명 중복 검증
         userService.validUserName(createUserReq.getUserName());
         // 캐릭터 ID 존재 검증
