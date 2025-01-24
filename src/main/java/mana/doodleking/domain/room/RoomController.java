1c75a681c8cb2dc24c8ed07f64ef42a7235ec0f5
syj <syj010820@naver.com>
2025-01-24 16:19:44 +0900

    docs: RoomController 문서 작성

--- src/main/java/mana/doodleking/domain/room/RoomController.java
+++ src/main/java/mana/doodleking/domain/room/RoomController.java
@@ -5,14 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import mana.doodleking.domain.room.dto.RoomDetail;
 import mana.doodleking.domain.room.dto.PostRoomReq;
 import mana.doodleking.domain.room.dto.RoomSimple;
-import mana.doodleking.domain.user.dto.CreateUserRes;
 import mana.doodleking.global.MessageSender;
-import mana.doodleking.global.response.APIResponse;
-import mana.doodleking.websocket.RequestChatContentsDto;
+import mana.doodleking.global.swagger.RoomControllerDocs;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,19 +16,17 @@ import java.util.List;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/api/room")
-@RestController
 @Slf4j
-public class RoomController {
+public class RoomController implements RoomControllerDocs {
     private final RoomService roomService;
     private final MessageSender messageSender;
-    @GetMapping
+    @GetMapping("/api/v1/room")
     public List<RoomSimple> getRoomList() {
         return roomService.getRoomList();
     }
 
     @MessageMapping("/createRoom")
-    public void sendMessage(@Header("userId") Long userId, PostRoomReq postRoomReq) {
+    public void createRoom(@Header("userId") Long userId, PostRoomReq postRoomReq) {
         try {
             RoomDetail createdRoom = roomService.createRoom(userId, postRoomReq);
             messageSender.send("/queue/user/" + userId, createdRoom);
