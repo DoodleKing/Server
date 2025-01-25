64d364028afb63508f88a264d51b4ea1d2915791
KSJ0128 <ksj01128@naver.com>
2025-01-25 20:49:34 +0900

    refact: 정적 리소스 경로 명시
    WebConfig에서 addResourceHandlers를 통해 정적 리소스 경로를 명시합니다.

--- src/main/java/mana/doodleking/global/config/WebConfig.java
+++ src/main/java/mana/doodleking/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package mana.doodleking.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")  // 허용할 헤더
                 .allowCredentials(true)  // 인증정보 허용
                 .maxAge(3600);  // CORS 응답 캐시 시간 (초)
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
