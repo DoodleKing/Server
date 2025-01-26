4080f66e18453dd5439cefc7209924d30579e644
KSJ0128 <ksj01128@naver.com>
2025-01-26 21:37:13 +0900

    feat: 방 상태 변경 및 redis 저장
    게임 방 상태를 PLAY로 변경합니다.
    또한, redis에 Game 설정 정보를 저장합니다.

--- src/main/java/mana/doodleking/domain/game/GameService.java
+++ src/main/java/mana/doodleking/domain/game/GameService.java
@@ -4,11 +4,20 @@ import lombok.RequiredArgsConstructor;
 import mana.doodleking.domain.room.RoomService;
 import mana.doodleking.domain.room.UserRoomService;
 import mana.doodleking.domain.room.domain.Room;
+import mana.doodleking.domain.room.domain.UserRoom;
+import mana.doodleking.domain.room.enums.RoomState;
 import mana.doodleking.domain.room.repository.RoomRepository;
+import mana.doodleking.domain.room.repository.UserRoomRepository;
 import mana.doodleking.domain.user.domain.User;
 import mana.doodleking.domain.user.repository.UserRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +26,8 @@ public class GameService {
     private final UserRoomService userRoomService;
     private final RoomRepository roomRepository;
     private final UserRepository userRepository;
+    private final UserRoomRepository userRoomRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
     private static final String PREFIX_GAME = "GAME";
     private static final String PREFIX_TURN = "TURN";
 
@@ -26,11 +37,31 @@ public class GameService {
         Room startRoom = roomRepository.findByIdOrThrow(startGameInfo.getId());
 
         canStartGame(startRoom, user);
+        startRoom.setRoomState(RoomState.PLAY);
 
+        setRedis(startRoom);
     }
 
     private void canStartGame(Room startRoom, User user) {
         roomService.checkRoomStatus(startRoom);
         userRoomService.checkUserStatus(startRoom, user);
+    }
+
+    private void setRedis(Room startRoom) {
+        redisTemplate.opsForValue()
+                .set(PREFIX_GAME + startRoom.getId(),
+                        RedisGameDTO.from(startRoom, createScore(startRoom)),
+                        Duration.ofSeconds((startRoom.getTime() * startRoom.getRound())));
+    }
+
+    private List<Map<String, Long>> createScore(Room room) {
+        return userRoomRepository.findAllByRoom(room).stream()
+                .map(UserRoom::getUser)
+                .map(user -> {
+                    Map<String, Long> scoreMap = new HashMap<>();
+                    scoreMap.put(user.getName(), 0L);
+                    return scoreMap;
+                })
+                .toList();
     }
 }
