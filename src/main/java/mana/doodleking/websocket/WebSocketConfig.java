229da0437808084739a547091d9bb19f75af2205
syj <syj010820@naver.com>
2025-01-27 22:23:43 +0900

    chore: websocket cors 허용

--- src/main/java/mana/doodleking/websocket/WebSocketConfig.java
+++ src/main/java/mana/doodleking/websocket/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://127.0.0.1:5500")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
