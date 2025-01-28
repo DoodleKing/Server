225b36595c80c5f1c0d4c818a63ff08caa7f457d
KSJ0128 <ksj01128@naver.com>
2025-01-28 16:29:04 +0900

    feat: startGame 로직 구현
    startGame 로직을 구현했습니다.
    방 생성 후 해당 방에 속한 참여자, 로비의 참여자에게 방 변경 사항을 메시지 발송합니다.

--- src/main/java/mana/doodleking/domain/game/GameController.java
+++ src/main/java/mana/doodleking/domain/game/GameController.java
@@ -1,17 +1,43 @@
 package mana.doodleking.domain.game;
 
+import jakarta.persistence.OptimisticLockException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mana.doodleking.domain.room.RoomService;
+import mana.doodleking.domain.room.dto.RoomIdDTO;
+import mana.doodleking.domain.room.dto.RoomSimple;
+import mana.doodleking.global.MessageSender;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
+@Controller
 public class GameController {
-    private GameService gameService;
+    private final GameService gameService;
+    private final RoomService roomService;
+    private final MessageSender messageSender;
 
     @MessageMapping("/startGame")
-    public void startGame(@Header("userId") Long userId, @Valid StartGameInfo startGameInfo) {
-        gameService.startGame(userId, startGameInfo);
-        // 초 카운팅?
+    public void startGame(@Header("userId") Long userId, @Valid RoomIdDTO roomIdDTO) {
+        try {
+            gameService.startGame(userId, roomIdDTO);
+            messageSender.send("/topic/room/" + roomIdDTO.getRoomId(), "Start GAME");
+
+            List<RoomSimple> roomList = roomService.getRoomList();
+            messageSender.send("/topic/lobby", roomList);
+        }
+        catch (OptimisticLockException e) {
+            log.warn(e.getMessage());
+            messageSender.sendError("/queue/user/" + userId, "동시성 문제 발생: 다시 시도해주세요.");
+        }
+        catch (Exception e) {
+            log.warn(e.getMessage());
+            messageSender.sendError("/queue/user/" + userId, e.getMessage());
+        }
     }
 }
